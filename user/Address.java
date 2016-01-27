dc7df91c625688527d6ca73a075752b88b86517a
Georgi K. Georigiev <gkalchv@icloud.com>
2016-01-27 22:14:13 +0200

    Revert "test 2"
    This reverts commit 715ec17a8a7545e9bbcc102a98193faf739ed404.

--- user/Address.java
+++ user/Address.java
@@ -15,77 +15,11 @@ public class Address {
 	 * @param postalCode
 	 * @param telephoneNumber
 	 */
-	public Address(String contactName, String city, String addressLine1, String addressLine2, String postalCode,String telephoneNumber) {
-		if(contactName!=null&&city!=null&&addressLine1!=null&&addressLine2!=null&&postalCode!=null&&telephoneNumber!=null){
-			this.contactName = contactName;
-			this.city = city;
-			this.addressLine1 = addressLine1;			
-			this.addressLine2 = addressLine2;			
-			this.postalCode = postalCode;
-			this.telephoneNumber = telephoneNumber;
-		}
-		else{
-			System.out.println("Invalid input");
-			return;
-		}
-		
-		
-		
-	}
-	public String getContactName() {
-		return contactName;
-	}
-	public void setContactName(String contactName) {
-		if(contactName!=null)
-			this.contactName = contactName;
-		else
-			System.out.println("You must enter a contact name!");
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		if(contactName!=null)
-			this.city = city;	
-		else
-			System.out.println("You must enter city!");
-	
-	}
-	public String getAddressLine1() {
-		return addressLine1;
-	}
-	public void setAddressLine1(String addressLine1) {
-		if(addressLine1!=null)
+	public Address(String contactName, String city, String addressLine1, String addressLine2, String postalCode,
 		this.addressLine1 = addressLine1;
-		else
-			System.out.println("You must enter Address Linea1!");
-	}
-	public String getAddressLine2() {
-		return addressLine2;
-	}
-	public void setAddressLine2(String addressLine2) {
-		if(addressLine2!=null)
 		this.addressLine2 = addressLine2;
-		else
-			System.out.println("You must enter Address Linea2");
-	}
-	public String getPostalCode() {
-		return postalCode;
-	}
-	public void setPostalCode(String postalCode) {
-		if(postalCode!=null)
-			this.postalCode = postalCode;
-		else
-			System.out.println("You must enter Postal Code!");
-	}
-	public String getTelephoneNumber() {
-		return telephoneNumber;
-	}
-	public void setTelephoneNumber(String telephoneNumber) {
-		if(telephoneNumber!=null)
-			this.telephoneNumber = telephoneNumber;
-		else
-			System.out.println("You must enter Telephone Number!");
+		this.postalCode = postalCode;
+		this.telephoneNumber = telephoneNumber;
 	}
 	
 	
